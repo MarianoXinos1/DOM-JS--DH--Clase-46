9d2f418552a57fd623d5cc675c9d2d29e3ac36fc
Mariano Xinos <mxinos@hotmail.com>
2024-05-27 18:52:40 -0300

    Add: Validacion si salio bien

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -20,11 +20,13 @@ let inputFields = document.querySelectorAll('input');
 let errorMessages = document.querySelectorAll('.error-message');
 
 
+
 inputFields.forEach((inputField, index) => {
     inputField.addEventListener('input', function(evento) {  // Evento input: se activa cada vez que el valor de un input cambia, validacion en tiempo real para el usuariio.
         if (inputField.value == "") {
             inputField.classList.add('is-invalid');                          // la agrego y saco porque es en tiempo real la validacion (no es necesaria si no)
             errorMessages[index].textContent = 'Este campo es obligatorio';
+
         } else {
             inputField.classList.remove('is-invalid');                       // la agrego y saco porque es en tiempo real la validacion (no es necesaria si no)
             inputField.classList.add('is-valid');   
@@ -33,7 +35,7 @@ inputFields.forEach((inputField, index) => {
 
         if (inputField.name == 'rating' || inputField.name == 'awards') {
             let value = parseFloat(inputField.value);                       // parseFloat: convierte un string a un número decimal.
-            if (value < 0 || value > 10) {
+            if (isNaN(value) || value < 0 || value > 10) {                  // isNan: devuelve true si el valor no es un número.
                 inputField.classList.add('is-invalid');
                 inputField.classList.remove('is-valid');
                 errorMessages[index].textContent = 'El valor debe estar entre 0 y 10';
@@ -54,7 +56,7 @@ inputFields.forEach((inputField, index) => {
                 inputField.classList.add('is-valid'); 
             }
         }
-
+        ulErrors.classList.add('alert-warning');
 
 
     });
@@ -73,9 +75,15 @@ formulario.addEventListener('submit', function(evento){
         }
     });
 
+    let invalidInputs = document.querySelectorAll('.is-invalid');
+
     if (EmptyField) {
         alert('Todos los campos son obligatorios');
+    } else {
+        alert('La película se guardó satisfactoriamente');
     }
+
+    
 });
 
 
