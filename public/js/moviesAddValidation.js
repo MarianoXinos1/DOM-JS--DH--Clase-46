53bfec79a38bc94a2dd8ff77f3ea548df4682b51
Mariano Xinos <mxinos@hotmail.com>
2024-05-26 23:36:45 -0300

    Add: Validacion Frontend: todos los campos son obligatorios

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -10,6 +10,25 @@ window.onload = function(){
 //------DESDE AQUÍ CONTINÚE CON LAS VALIDACIONES DEL FORMULARIO //
 //-------------------DE REGISTRO DE PELÍCULAS------------------//    
 
+console.log("El script se está cargando");    // Se ve solo Consola Cliente
+let inputTitle = document.querySelector('#title');
+inputTitle.focus();
+
+formulario.addEventListener('submit', function(evento){
+
+    let inputFields = document.querySelectorAll('input');
+
+    for (let i = 0; i < inputFields.length; i++) {
+        console.log(inputFields[i].value);
+        if (inputFields[i].value == "") {
+            evento.preventDefault();
+            alert('Todos los campos son obligatorios');
+            return
+        }
+    }
+})
+
+
 
 
 
