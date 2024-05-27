1e004e39b0f1954923643b6d512c6798e9fd0125
Mariano Xinos <mxinos@hotmail.com>
2024-05-27 14:38:17 -0300

    Add: Validaciones especiales numericas

--- public/js/moviesAddValidation.js
+++ public/js/moviesAddValidation.js
@@ -21,14 +21,31 @@ let errorMessages = document.querySelectorAll('.error-message');
 
 
 inputFields.forEach((inputField, index) => {
-    inputField.addEventListener('input', function(evento) {   // Evento input: se dispara cada vez que el valor de un input cambia, validacion tiempo real para el usuariio.
+    inputField.addEventListener('input', function(evento) {   // Evento input: se activa cada vez que el valor de un input cambia, validacion en tiempo real para el usuariio.
         if (inputField.value == "") {
             inputField.classList.add('is-invalid');
             errorMessages[index].textContent = 'Este campo es obligatorio';
         } else {
             inputField.classList.remove('is-invalid');
-            errorMessages[index].textContent = 'Este campo es obligatorio';
+            errorMessages[index].textContent = '';  // Para limpia el mensaje de error.
         }
+
+        if (inputField.name == 'rating' || inputField.name == 'awards') {
+            let value = parseFloat(inputField.value);    // parseFloat: convierte un string a un número decimal.
+            if (value < 0 || value > 10) {
+                inputField.classList.add('is-invalid');
+                errorMessages[index].textContent = 'El valor debe estar entre 0 y 10';
+            } 
+        }
+
+        if (inputField.name == 'length') {
+            let value = parseInt(inputField.value);  // parseInt: convierte un string a un número entero.
+            if (value < 60 || value > 360) {
+                inputField.classList.add('is-invalid');
+                errorMessages[index].textContent = 'La duración debe estar entre 60 y 360 minutos';
+            }
+        }
+
     });
 });
 
